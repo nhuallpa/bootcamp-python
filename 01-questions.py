5ea968c5a564a966e3b6a84fe9298bcad190b2a2
nestor.huallpa <huallpa.nestor@gmail.com>
2022-05-22 23:50:15 -0300

    First program

---
+++ 01-questions.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator")
+city_name = input("What is name of the city you grew up in ?")
+pet_name = input("What is your pet name?")
+print("your band name could be " + city_name + " " + pet_name)
